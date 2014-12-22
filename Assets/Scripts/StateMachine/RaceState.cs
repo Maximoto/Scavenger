5a821cc3a315f777b840618a596962cdb0634825
Ian Christensen <christensen.ian.t@gmail.com>
2014-12-21 18:24:40 -0800

    Remove wacky color changes.

--- Assets/Scripts/StateMachine/RaceState.cs
+++ Assets/Scripts/StateMachine/RaceState.cs
@@ -23,7 +23,6 @@ public class RaceState : State
 
     bool updateThing = false;
     bool updateReturn = false;
-    float updateBackground = 0;
 
     public RaceState(StateMachine s)
         : base(s)
@@ -72,8 +71,6 @@ public class RaceState : State
         updateReturn = true;
         yield return new WaitForSeconds(2.0f);
         updateReturn = false;
-        updateBackground = 3.0f;
-        yield return new WaitForSeconds(3.0f);
         
         foreach(Player p in players.Values)
         {
@@ -95,16 +92,6 @@ public class RaceState : State
         if(updateReturn)
         {
             returnText.text = returns[Random.Range(0, returns.Length)];
-        }
-        if(updateBackground > 1.5f)
-        {
-            SceneObjects["Background"].GetComponent<Image>().color = Color.Lerp(Color.red, Color.yellow, (3.0f - updateBackground) / 1.5f);
-            updateBackground -= Time.deltaTime;
-        }
-        else if(updateBackground > 0.0f)
-        {
-            SceneObjects["Background"].GetComponent<Image>().color = Color.Lerp(Color.yellow, Color.green, (1.5f - updateBackground) / 1.5f);
-            updateBackground -= Time.deltaTime;
         }
     }
 
